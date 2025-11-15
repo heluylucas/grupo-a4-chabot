920b4ff62b8a9df5b2a6735988af20d082f37505
heluylucas <heluylucas@gmail.com>
2025-11-15 13:41:33 -0300

    :bug: fix: width

--- src/templates/loading/styles.ts
+++ src/templates/loading/styles.ts
@@ -7,7 +7,6 @@ export const MainContainer = styled.div<{ height?: number }>`
     align-items: center;
     justify-content: center;
 
-    width: 100%;
     height: ${(props) => (props.height ? `${props.height}px` : 'auto')};
     background-color: ${theme.colors.primary.background};
 `;
